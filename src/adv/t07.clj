f12fd5144410e568b21f53dd57d6a558d6413f38
Maxim Fainberg <maksym.fainberg@mobenga.com>
2020-02-09 23:47:45 +0100

    Day 7 - part 2

--- src/adv/t07.clj
+++ src/adv/t07.clj
@@ -39,10 +39,11 @@
            (v (+ pos 3)) 
            (if (pred a b) 1 0))))
 
+(def pause-on-output true)
 (defn execute [v pos in-params out-param]
   (let [op (mod (v pos) 100)
         modes (quot (v pos) 100)]
-    (println "execute" pos op in-params out-param)
+    ;(println "execute" pos op in-params out-param)
     (condp = op
       99 {:out out-param, :data v}
       1 (recur (run-binary-op v pos + modes) (+ pos 4) in-params nil)       
@@ -53,10 +54,15 @@
                  (rest in-params)
                  nil)
           (throw (Exception. "no input")))
-      4 (recur v 
-               (+ pos 2)
-               in-params
-               (get-param v (inc pos) modes)) 
+      4 (if pause-on-output
+          {:out (get-param v (inc pos) modes)
+           :data v
+           :pos (+ pos 2)
+           :suspended true}
+          (recur v 
+                 (+ pos 2)
+                 in-params
+                 (get-param v (inc pos) modes))) 
       5 (recur v 
                (jump-if-next-pos true v pos modes)
                in-params
@@ -78,8 +84,8 @@
 ;(def input [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
 ;(def input [3,23,3,24,1002,24,10,24,1002,23,-1,23,
 ;101,5,23,23,1,24,23,23,4,23,99,0,0])
-(def input [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-  27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
+;(def input [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+;  27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
 ;(def input [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
 ;-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
 ;53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10])
@@ -99,30 +105,31 @@
 
 (defn run-loop 
   ([data phase-settings] 
-    (run-loop (vec (repeat 5 data)) 0 (vec phase-settings) 0 nil))
-  ([datas inp phase-settings idx curr-signal] (println "run-loop" idx) 
-    (let [{:keys [out data]} (execute (datas idx) 
-                                      0
-                                      ;(list (phase-settings idx) inp))]
-                                      (if (seq phase-settings)
-                                        (list (first phase-settings) inp)
-                                        (list inp))
-                                      nil)]
-      (if (nil? out)
-        curr-signal
-        (recur (assoc datas idx data)
+    (run-loop (vec (repeat 5 {:data data, :pointer 0})) 0 (vec phase-settings) 0 nil))
+  ([datas inp phase-settings idx curr-signal] ;(println "run-loop" idx) 
+    (let [{:keys [out data pos suspended]} 
+      (execute (:data (datas idx))
+               (:pointer (datas idx))
+               ;(list (phase-settings idx) inp))]
+               (if (seq phase-settings)
+                 (list (first phase-settings) inp)
+                 (list inp))
+               nil)]
+      (if suspended
+        (recur (assoc datas idx {:data data, :pointer pos})
                out
                (rest phase-settings)
                (mod (inc idx) 5)
-               (if (= idx 4) out curr-signal))))))
+               (if (= idx 4) out curr-signal))
+        curr-signal))))
 
 (defn solve2 
   ([] (solve2 input))
   ([data] 
-    (->> ;[5 6 7 8 9]
-         ;(permutations)
-         [[9 7 8 5 6]]
+    (->> [5 6 7 8 9]
+         (permutations)
+         ;[[9 7 8 5 6]]
          (map (partial run-loop data))
-         ;(apply max)
+         (apply max)
          )))
 
